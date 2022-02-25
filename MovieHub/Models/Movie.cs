d4a85802090d3df141c66882b507a3c2e8585452
Rob vd H <accounts.rob@mailbox.org>
2022-02-25 15:45:24 +0100

    Basis Index Pagina

--- MovieHub/Models/Movie.cs
+++ MovieHub/Models/Movie.cs
@@ -14,7 +14,7 @@ public class Movie
     [DisplayName("IMDb Score")]
     public double ImdbScore { get; set; } // In stead of Stars
     [DisplayName("Release date")]
-    public DateOnly ReleaseDate { get; set; } 
+    public DateTime ReleaseDate { get; set; } 
     [DisplayName("3D")]
     public bool Is3D { get; set; }
     [DisplayName("Secret")]
