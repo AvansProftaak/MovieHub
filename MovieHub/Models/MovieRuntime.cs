914a114fb8200d4d86a0be9f5c11801a76fd0bc3
Rob vd H <accounts.rob@mailbox.org>
2022-02-27 08:35:38 +0100

    Index aanpassing

---
+++ MovieHub/Models/MovieRuntime.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieHub.Models;
+
+public class MovieRuntime
+{
+        public int Id { get; set; }
+        public int MovieId { get; set; }
+        public int HallId { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        public DateTime StartAt { get; set; }
+
+        [DataType(DataType.Date)]
+        [Column(TypeName = "Date")]
+        public DateTime EndAt { get; set; }
+
+        [DataType(DataType.Time)]
+        [Column(TypeName = "Time")]
+        public TimeSpan Time { get; set; }
+
+        [ForeignKey("MovieId")] 
+        public virtual Movie Movie { get; set; } = null!;
+        [ForeignKey("HallId")] 
+        public virtual Hall Hall { get; set; } = null!;
+
+        public MovieRuntime()
+        {
+                
+        }
+
+}
