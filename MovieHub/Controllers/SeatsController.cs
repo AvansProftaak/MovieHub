6217726bee142620892b51ddd0ad6a6090bdc939
zafuso <71095771+zafuso@users.noreply.github.com>
2022-02-28 08:50:15 +0100

    start on seatmap

---
+++ MovieHub/Controllers/SeatsController.cs
@@ -0,0 +1,161 @@
+#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Data;
+using MovieHub.Models;
+
+namespace MovieHub.Controllers
+{
+    public class SeatsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SeatsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Seats
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Seat.Where(s => s.HallId = 1).Include(s => s.Hall);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Seats/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seat = await _context.Seat
+                .Include(s => s.Hall)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return View(seat);
+        }
+
+        // GET: Seats/Create
+        public IActionResult Create()
+        {
+            ViewData["HallId"] = new SelectList(_context.Hall, "Id", "Id");
+            return View();
+        }
+
+        // POST: Seats/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,RowNumber,SeatNumber,Available,HallId")] Seat seat)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(seat);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Hall, "Id", "Id", seat.HallId);
+            return View(seat);
+        }
+
+        // GET: Seats/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seat = await _context.Seat.FindAsync(id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+            ViewData["HallId"] = new SelectList(_context.Hall, "Id", "Id", seat.HallId);
+            return View(seat);
+        }
+
+        // POST: Seats/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,RowNumber,SeatNumber,Available,HallId")] Seat seat)
+        {
+            if (id != seat.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(seat);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SeatExists(seat.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["HallId"] = new SelectList(_context.Hall, "Id", "Id", seat.HallId);
+            return View(seat);
+        }
+
+        // GET: Seats/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var seat = await _context.Seat
+                .Include(s => s.Hall)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (seat == null)
+            {
+                return NotFound();
+            }
+
+            return View(seat);
+        }
+
+        // POST: Seats/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var seat = await _context.Seat.FindAsync(id);
+            _context.Seat.Remove(seat);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SeatExists(int id)
+        {
+            return _context.Seat.Any(e => e.Id == id);
+        }
+    }
+}
