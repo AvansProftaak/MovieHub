16304545c95d0a301a9e3a06d53012d157400c96
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2022-03-07 17:35:14 +0100

    add some initial things

--- MovieHub/Controllers/OrdersController.cs
+++ MovieHub/Controllers/OrdersController.cs
@@ -1,12 +1,32 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Data;
 
 namespace MovieHub.Controllers;
 
 public class OrdersController : Controller
 {
+    
+    private readonly ILogger<OrdersController> _logger;
+    private readonly ApplicationDbContext _context;
+
+    public OrdersController(ILogger<OrdersController> logger,
+        ApplicationDbContext context)
+    {
+        _logger = logger;
+        _context = context;
+    }
+    
     // GET
     public IActionResult Index()
     {
         return View();
+    }
+
+    public async Task<IActionResult> Payment(int orderId)
+    {
+        var payment = await _context.Payment
+            .FirstOrDefaultAsync(p => p.OrderId == orderId);
+        return View(payment);
     }
 }
