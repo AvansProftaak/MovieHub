35e5b0b93656d6d18ef118008f0e38d6e081a5cc
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2022-03-24 13:15:24 +0100

    QR-code

---
+++ MovieHub/Migrations/20220324121103_PaymentMethod.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MovieHub.Migrations
+{
+    public partial class PaymentMethod : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Seat_SeatId",
+                table: "Ticket");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatId",
+                table: "Ticket",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Seat_SeatId",
+                table: "Ticket",
+                column: "SeatId",
+                principalTable: "Seat",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Ticket_Seat_SeatId",
+                table: "Ticket");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SeatId",
+                table: "Ticket",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Ticket_Seat_SeatId",
+                table: "Ticket",
+                column: "SeatId",
+                principalTable: "Seat",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
