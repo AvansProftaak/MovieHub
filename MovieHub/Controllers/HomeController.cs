15a86de6ff19f3f6113e6a634db72d285734c8ad
zafuso <71095771+zafuso@users.noreply.github.com>
2022-03-29 12:39:36 +0200

    start with clean migration

--- MovieHub/Controllers/HomeController.cs
+++ MovieHub/Controllers/HomeController.cs
@@ -4,7 +4,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
 using Microsoft.Extensions.Logging;
 using MovieHub.Data;
-using MovieHub.Migrations;
 using MovieHub.Models;
 using MovieHub.ViewModels;
 
--- MovieHub/Models/Genre.cs
+++ MovieHub/Models/Genre.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using System.ComponentModel;
-using MovieHub.Migrations;
 
 namespace MovieHub.Models;
 
