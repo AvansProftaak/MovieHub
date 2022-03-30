5a661dde52c94deb7c589ca7347c0e7a98f9a7d9
zafuso <71095771+zafuso@users.noreply.github.com>
2022-03-30 13:21:31 +0200

    add role 3

--- MovieHub/ViewModels/AddRoleViewModel.cs
+++ MovieHub/ViewModels/AddRoleViewModel.cs
@@ -8,6 +8,7 @@ public class AddRoleViewModel
     [Required]
     [Display(Name = "Role")]
     public string? SelectedRole { get; set; }
+    public string? SelectedUserId { get; set; }
     
     public IEnumerable<string>? AvailableRoles { get; set; }
     public IEnumerable<IdentityUser>? Users { get; set; }
