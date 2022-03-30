abb40cddaecc45dacdf313d9e0079d89a497dc95
zafuso <71095771+zafuso@users.noreply.github.com>
2022-03-30 12:25:20 +0200

    working on eiditing roles

--- MovieHub/Controllers/BackOfficeController.cs
+++ MovieHub/Controllers/BackOfficeController.cs
@@ -1,14 +1,35 @@
 ﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using MovieHub.Data;
 
 namespace MovieHub.Controllers;
 
 [Authorize]
 public class BackOfficeController : Controller
 {
+    private readonly ApplicationDbContext _context;
+    private readonly UserManager<IdentityUser> _userManager;
+
+    public BackOfficeController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+    {
+        _context = context;
+        _userManager = userManager;
+    }
     // GET
     public IActionResult Index()
     {
         return View();
+    }
+    
+    public IActionResult ManageUsers()
+    {
+        return View( _context.Users.ToList());
+    }
+
+    public void RemoveRole(IdentityUser user, string role)
+    {
+        _userManager.RemoveFromRoleAsync(user, role).Wait();
+        ManageUsers();
     }
 }
---
+++ MovieHub/ViewModels/UserViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MovieHub.ViewModels;
+
+public class UserViewModel
+{
+    public IdentityUser? IdentityUser { get; set; }
+    public List<string> Roles { get; set; }
+}
