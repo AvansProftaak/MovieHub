5dcdb138bbc2c4979664d65d59ecdc104823c6c5
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-02 21:05:15 +0200

    started on User management

--- MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -121,7 +121,7 @@ namespace MovieHub.Areas.Identity.Pages.Account
 
                 if (result.Succeeded)
                 {
-                    _userManager.AddToRoleAsync(user, "Admin").Wait();
+                    _userManager.AddToRoleAsync(user, "User").Wait();
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
---
+++ MovieHub/Controllers/UserManagementController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MovieHub.Controllers;
+
+[Authorize (Roles = "Admin")]
+public class UserManagementController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
