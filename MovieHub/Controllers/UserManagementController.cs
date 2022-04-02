952d75e401ed95670c3bd95e2b5bf6096d0a4858
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-02 22:14:04 +0200

    users can now be selected on edit page

--- MovieHub/Controllers/UserManagementController.cs
+++ MovieHub/Controllers/UserManagementController.cs
@@ -1,19 +1,57 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Linq;
+using System.Net.Mime;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.Framework;
+using Microsoft.EntityFrameworkCore;
+using MovieHub.Data;
+using MovieHub.Models;
 
 namespace MovieHub.Controllers;
 
 [Authorize (Roles = "Admin")]
 public class UserManagementController : Controller
 {
+    
+    private readonly RoleManager<IdentityRole> _roleManager;
+    private readonly UserManager<User> _userManager;
+    private readonly ApplicationDbContext _context;
+    
+    
+    public UserManagementController(RoleManager<IdentityRole> roleManager, UserManager<User> userManager, ApplicationDbContext context)
+    {
+        _roleManager = roleManager;
+        _userManager = userManager;
+        _context = context;
+    }
+    
+    
     // GET
     public IActionResult Index()
     {
-        return View();
+        
+        return View(GetUSers());
     }
     
-    public IActionResult Edit()
+    public IActionResult Edit(string userId)
     {
-        return View();
+        var taskUser = GetUSer(userId);
+        User? user = taskUser.Result;
+        
+        return View(user);
+    }
+
+    public ICollection<User> GetUSers()
+    {
+        return _context.Users.ToList();
+    }
+
+    public Task<User?> GetUSer(string userId)
+    {
+        
+        var user = _context.User
+            .FirstOrDefaultAsync(u => u.Id == userId);
+        return user;
     }
 }
