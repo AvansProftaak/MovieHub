72bdef72fe3bda1c3589057a2db75a4509dd3567
Rob vd H <accounts.rob@mailbox.org>
2022-04-04 18:27:45 +0200

    getMovies tbv Test

--- MovieHub/Controllers/HomeController.cs
+++ MovieHub/Controllers/HomeController.cs
@@ -29,49 +29,12 @@ public class HomeController : Controller
         return Task.FromResult<ActionResult<IndexViewModel>>(View(indexViewModel));
         }
     
-
-    public List<Showtime> ShowNext()
-    {
-        return _context.Showtime!
-                 .FromSqlRaw("SELECT x.* FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY \"HallId\" ORDER BY \"StartAt\") rn FROM \"Showtime\" where \"StartAt\" > now()) x JOIN \"Movie\" M ON \"MovieId\" = M.\"Id\" WHERE x.rn = 1 ORDER BY \"HallId\"").ToList();
-    }
-
-    public List<Showtime> ShowNow()
-    {
-        return _context.Showtime!
-            .FromSqlRaw(
-                "SELECT x.* FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY \"HallId\" ORDER BY \"StartAt\" DESC ) rn FROM \"Showtime\" WHERE \"StartAt\" < now()) x JOIN \"Movie\" M ON \"MovieId\" = M.\"Id\" WHERE x.rn = 1 ORDER BY \"HallId\"").ToList();
-    }
-    
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
     }
-
-    private static DateTime GetFirstDayOfWeek(DateTime date)
-    {
-        const DayOfWeek firstDay = DayOfWeek.Monday;
-
-        var diff = date.DayOfWeek - firstDay;
-        if (diff < 0)
-            diff += 7;
-        return date.AddDays(-diff).Date;
-    }
-
-    //To Get The Last Day of the Week in C#
-    private static DateTime GetLastDayOfWeek(DateTime date)
-    {
-        const DayOfWeek firstDay = DayOfWeek.Monday;
-        var diff = date.DayOfWeek - firstDay;
-
-        if (diff < 0)
-            diff += 7;
-        var start = date.AddDays(-diff).Date;
-        
-        // Add 6 days to get the last day, but to display all movies from the last day we add 7!
-        return start.AddDays(7).Date;
-    }
+    
     
     public async Task<IActionResult> InsertEmail(string email)
     {
@@ -90,5 +53,10 @@ public class HomeController : Controller
         await _context.Newsletter.AddAsync(newsletter);
         await _context.SaveChangesAsync();
         return Ok();
+    }
+
+    public List<Movie> GetMovies()
+    {
+        return _context.Movie.ToList();
     }
 }
--- MovieHub/ViewModels/MovieRuntimeViewModel.cs
+++ MovieHub/ViewModels/MovieRuntimeViewModel.cs
@@ -5,8 +5,5 @@ namespace MovieHub.ViewModels;
 public class MovieRuntimeViewModel
 {
     public List<MovieRuntime> RuntimeList { get; set; }
-    public MovieRuntime MovieRuntime { get; set; }
-    public Movie Movie { get; set; }
     public List<Hall> Halls { get; set; }
-    public Showtime Showtime { get; set; }
 }
