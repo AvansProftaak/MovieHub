ec037a911e09c3d339bda5ffec4c904ecd943de2
zafuso <71095771+zafuso@users.noreply.github.com>
2022-04-05 19:03:01 +0200

    add survey mvc controller and views

--- MovieHub/Models/Survey.cs
+++ MovieHub/Models/Survey.cs
@@ -1,11 +1,16 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MovieHub.Models;
 
 public class Survey
 {
     public int Id { get; set; }
+    [StringLength(60, MinimumLength = 3)]
+    [Required]
     public string? Title { get; set; }
+    [StringLength(255, MinimumLength = 3)]
+    [Required]
     public string? Description { get; set; }
     [DisplayName("Active")]
     public bool IsActive { get; set; }
