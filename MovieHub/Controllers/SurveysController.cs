e29786bf2122bb0a5160ffafbe31eccfb0bd1bce
zafuso <71095771+zafuso@users.noreply.github.com>
2022-04-05 18:49:36 +0200

    start on create survey field

---
+++ MovieHub/Controllers/SurveysController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MovieHub.Controllers;
+
+public class SurveysController : Controller
+{
+    // GET
+    public IActionResult CreateSurvey()
+    {
+        return View();
+    }
+}
