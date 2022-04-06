4615973e05cd73bd93accf89ce2360da2a702bd4
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-06 12:23:56 +0200

    review now in db

---
+++ MovieHub/Migrations/20220406102305_reviewToDb.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MovieHub.Migrations
+{
+    public partial class reviewToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    DisplayQuality = table.Column<int>(type: "integer", nullable: false),
+                    SoundQuality = table.Column<int>(type: "integer", nullable: false),
+                    FoodQuality = table.Column<int>(type: "integer", nullable: false),
+                    Disturbance = table.Column<int>(type: "integer", nullable: false),
+                    Hygiene = table.Column<int>(type: "integer", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    CinemaId = table.Column<int>(type: "integer", nullable: false),
+                    HallId = table.Column<int>(type: "integer", nullable: false),
+                    TimeCreated = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Review_Cinema_CinemaId",
+                        column: x => x.CinemaId,
+                        principalTable: "Cinema",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Review_Hall_HallId",
+                        column: x => x.HallId,
+                        principalTable: "Hall",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_CinemaId",
+                table: "Review",
+                column: "CinemaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_HallId",
+                table: "Review",
+                column: "HallId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Review");
+        }
+    }
+}
