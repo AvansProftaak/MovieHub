6e574e72af7c678f47224b68e75d32a40f335753
zafuso <71095771+zafuso@users.noreply.github.com>
2022-04-07 16:07:40 +0200

    showstats

---
+++ MovieHub/Models/ShowStats.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieHub.Models;
+
+[NotMapped]
+public class ShowStats
+{
+    public DateTime Showtime { get; set; }   
+    public string MovieTitle { get; set; }
+    public string Hall { get; set; }
+    public int SeatsFree { get; set; }
+
+    public ShowStats()
+    {
+        
+    }
+}
--- MovieHub/ViewModels/AnalyticsViewModel.cs
+++ MovieHub/ViewModels/AnalyticsViewModel.cs
@@ -9,4 +9,5 @@ public class AnalyticsViewModel
     public IEnumerable<HallAnalytics>? Statistics { get; set; }
     public IEnumerable<MovieRevenue>? MovieStatistics { get; set; }
     public IEnumerable<SurveyResults>? SurveyResults { get; set; }
+    public IEnumerable<ShowStats>? ShowStats { get; set; }
 }
