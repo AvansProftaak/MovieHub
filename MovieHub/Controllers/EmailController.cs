aa3c93a62404fbab89f11f6cd4b8f8db87d2b92f
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-07 12:59:10 +0200

    testing started

--- MovieHub/Controllers/EmailController.cs
+++ MovieHub/Controllers/EmailController.cs
@@ -53,117 +53,7 @@ namespace MovieHub.Controllers
         {
             return View();
         }
-
-        // POST: Email/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        /*[HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Content")] Email email)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(email);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(email);
-        }
-
-        // GET: Email/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var email = await _context.Email.FindAsync(id);
-            if (email == null)
-            {
-                return NotFound();
-            }
-            return View(email);
-        }
-
-        // POST: Email/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Content")] Email email)
-        {
-            if (id != email.Id)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(email);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!EmailExists(email.Id))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            return View(email);
-        }*/
-
-        // GET: Email/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var email = await _context.Email
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (email == null)
-            {
-                return NotFound();
-            }
-
-            return View(email);
-        }
-
-        // POST: Email/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            var email = await _context.Email.FindAsync(id);
-            _context.Email.Remove(email);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool EmailExists(int id)
-        {
-            return _context.Email.Any(e => e.Id == id);
-        }
-
-
-
-
-
-
-
-
-
-
+        
         [HttpPost]
         public ActionResult Create([Bind("Subject,Content")] Email email)
         {
@@ -216,6 +106,41 @@ namespace MovieHub.Controllers
             return View();
 
         }
+        
+        // GET: Email/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var email = await _context.Email
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (email == null)
+            {
+                return NotFound();
+            }
+
+            return View(email);
+        }
+
+        // POST: Email/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var email = await _context.Email.FindAsync(id);
+            _context.Email.Remove(email);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EmailExists(int id)
+        {
+            return _context.Email.Any(e => e.Id == id);
+        }
+
     }
 }
 
---
+++ MovieHubTests/ReviewControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using MovieHub.Controllers;
+using MovieHub.Data;
+using MovieHub.Models;
+using Xunit;
+
+namespace MovieHubTests;
+
+public class ReviewControllerTests
+{
+    private readonly ReviewController _controller;
+    private readonly ApplicationDbContext _context;
+
+    public ReviewControllerTests()
+    {
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase("MovieTestDatabase").Options;
+        _context = new ApplicationDbContext(options);
+        _controller = new ReviewController(_context);
+    }
+    
+    [Fact]
+    public void Test_Review_Database_Ok()
+    {
+        _context.Database.EnsureCreated();
+        InsertTestData(_context);
+        Assert.Equal("Bart", _context.Review.First().Name);
+    }
+
+    [Fact]
+    public void Test_Details_Returns_Details_View()
+    {
+        var result = _controller.Details(1);
+        Assert.IsType<Task<IActionResult>>(result);
+    }
+
+    private void InsertTestData(ApplicationDbContext context)
+    {
+        context.Add(new Review()
+        {
+            Id = 1,
+            CinemaId = 1,
+            HallId = 2,
+            DisplayQuality = 4,
+            SoundQuality = 4,
+            Disturbance = 2,
+            FoodQuality = 4,
+            Hygiene = 5,
+            Email = "bart-grootoonk@hotmail.com",
+            Name = "Bart"
+
+        });
+
+        context.SaveChanges();
+    }
+}
+    
