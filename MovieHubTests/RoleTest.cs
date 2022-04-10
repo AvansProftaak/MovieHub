8dccff292a116e345da1dd8d1c2406694f738be5
Daan30474 <38464602+Daan30474@users.noreply.github.com>
2022-04-10 11:47:33 +0200

    Added RoleTest

---
+++ MovieHubTests/RoleTest.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using MovieHub.ViewModels;
+using Xunit;
+
+namespace MovieHubTests;
+
+public class RoleTest
+{
+
+    [Fact]
+    public void RoleFieldCannotBeBlank()
+    {
+        // Arrange
+        var validationResults = new List<ValidationResult>();
+
+        var sut = new AddRoleViewmodel()
+        {
+            RoleName = ""
+        };
+        var ctx = new ValidationContext(sut, null, null);
+
+        // Act
+        Validator.TryValidateObject(sut, ctx, validationResults, true);
+
+        // Assert
+        Assert.Single(validationResults);
+        Assert.Contains(validationResults,
+            validationResult => validationResult.ErrorMessage.Contains("The Role field is required"));
+
+    }
+
+    [Fact]
+    public void RoleNameFieldCannotBeBlank()
+    {
+        // Arrange
+        var validationResults = new List<ValidationResult>();
+
+        var sut = new EditRoleViewModel()
+        {
+            Id = "ID-1",
+            RoleName = ""
+        };
+        var ctx = new ValidationContext(sut, null, null);
+
+        // Act
+        Validator.TryValidateObject(sut, ctx, validationResults, true);
+
+        // Assert
+        Assert.Single(validationResults);
+        Assert.Contains(validationResults,
+            validationResult => validationResult.ErrorMessage.Contains("Role Name is required"));
+
+    }
+    
+}
