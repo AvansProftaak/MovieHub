0b0a003085d813c3fe1769d595d1d83fc8989696
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-11 20:09:48 +0200

    first try input first and last

--- MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -113,11 +113,17 @@ namespace MovieHub.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
+                var user = new User()
+                {
+                    UserName = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    Email = Input.Email,
+                };
+                var result = await _userManager.CreateAsync(user, Input.Password);
+                
                 var user = CreateUser();
 
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
-                await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                var result = await _userManager.CreateAsync(user, Input.Password);
 
                 if (result.Succeeded)
                 {
