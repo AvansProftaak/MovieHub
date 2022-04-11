b0d25b66e44e75e3249e69f6ee4bb6b94edeeb0e
Shabular <43166019+Shabular@users.noreply.github.com>
2022-04-11 20:22:06 +0200

    authorization now accepting first and last name

--- MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ MovieHub/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,29 +71,27 @@ namespace MovieHub.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "First Name")]
+            public string FirstName { get; set; }
+            
+            [Required]
+            [DataType(DataType.Text)]
+            [Display(Name = "Last Name")]
+            public string LastName { get; set; }
+            
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
-
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
+            
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
             [Display(Name = "Password")]
             public string Password { get; set; }
 
-            /// <summary>
-            ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-            ///     directly from your code. This API may change or be removed in future releases.
-            /// </summary>
             [DataType(DataType.Password)]
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
@@ -121,8 +119,7 @@ namespace MovieHub.Areas.Identity.Pages.Account
                     Email = Input.Email,
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                
-                var user = CreateUser();
+
 
 
                 if (result.Succeeded)
